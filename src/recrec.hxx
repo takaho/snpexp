a7fdaa1097c4089a4d4c2825bad9f9ca34b6743d
Takaho A. Endo <takaho.endo@riken.jp>
2014-08-29 02:37:25 +0900

    recrec.hxx

--- src/recrec.hxx
+++ src/recrec.hxx
@@ -14,15 +14,20 @@ namespace tkbio {
         int _position;
         int _flag;
         vector<pair<int,char> > _mapped;
-        
+        int _position5;
+        int _position3;
+        int _max_match_span;
+    private:
+        void initialize(int position, const string& sequence, const string& cigar);
+        vector<pair<int,char> > get_positions() const;
 
-        
     public:
         fragment(const string& chromosome, int position, int flag, const string& sequence, const string& cigar);
 
         int position() const { return _position; }
-        int length() const { return _length; }
-        char orientation() const { return (flag & 16) == 0 : '+' : '-'; }
+        int span() const { return _position3 - _position5 + 1; }
+        //int length() const { return _length; }
+        char orientation() const { return (_flag & 16) == 0 ? '+' : '-'; }
         char get_base(int position) const;
 
         static vector<pair<int,char> > resolve_map(int position, const string& cigar, const string& sequence);
