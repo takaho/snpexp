138b770ae9f874898d21203aa66072414fbc0f1c
Takaho A. Endo <takaho.endo@riekn.jp>
2014-09-01 18:11:07 +0900

    header

--- src/recrec.hxx
+++ src/recrec.hxx
@@ -12,7 +12,6 @@ namespace tkbio {
         string _name;
         string _sequence;
         string _cigar;
-        //string _chromosome;
         int _chromosome;
         int _position;
         int _flag;
@@ -21,16 +20,26 @@ namespace tkbio {
         int _position3;
         int _max_match_span;
     private:
+        // inhibit default and copy constructors
+        recfragment();
+        const recfragment& operator = (const recfragment&);
+        recfragment(const recfragment& rhs);
+
+        // initializers
         void initialize(int position, const string& sequence, const string& cigar);
         void initialize(bam1_t* sequence);
+
+        //
         vector<pair<int,char> > get_positions() const;
-        //static recfragment* join_pair(recfragment* f1, recfragment* f2);
         void join_sequence(const recfragment* frag);
         void generate_recombination_pattern(const vector<pair<int,char> >& loci, int* pattern) const;
-        recfragment();
+
     public:
-        recfragment(const string& chromosome, int position, int flag, const string& sequence, const string& cigar);
+        //recfragment(const string& chromosome, int position, int flag, const string& sequence, const string& cigar);
+        // constructor with a BAM read
         recfragment(int chromosome, bam1_t* sequence);
+
+        // properties
         const string& name() const { return _name; }
         int chromosome() const { return _chromosome; }
         int position() const { return _position; }
@@ -38,14 +47,15 @@ namespace tkbio {
         int position5() const { return _position5; }
         int position3() const { return _position3; }
         bool is_temporary() const {return _cigar == "";}
-        
-        //int length() const { return _length; }
         char orientation() const { return (_flag & 16) == 0 ? '+' : '-'; }
+
+        // getters
         char get_base(int position, int& count) const;
         void test_heterozygosity(int num, char const* reference, char* const alt, int* result) const;
 
+        // recombination test
         string get_recombination_pattern(const vector<pair<int,char> >& loci) const;
-        pair<int,int> get_recombination(const vector<pair<int,char> >& loci) const;
+        pair<int,int> get_recombination(const vector<pair<int,char> >& loci, float diff_ratio=0.0) const;
 
         // join pairs
         static void bundle_pairs(vector<recfragment*>& fragments) throw (runtime_error);
@@ -54,8 +64,6 @@ namespace tkbio {
         static void detect_recombination(const vector<recfragment*>& frags,
                                          int clength, char const* cseq,
                                          int start, int end) throw (exception);
-//                                         int start, int end) throw (exception);
-        //friend ostream& ::operator << (ostream& ost, const recfragment& rhs);
     };
 
 }
